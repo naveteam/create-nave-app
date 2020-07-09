eb2de4117b7dad1a52bd940a37d949081064b55a
Eduardo Bittencourt <eduardo.bittencourt@nave.rs>
2020-07-09 15:39:09 -0300

    create another boilerplates

--- index.js
+++ index.js
@@ -1,32 +1,68 @@
 #!/usr/bin/env node
 
 const shell = require('shelljs')
+const colors = require('colors')
+const argv = require('yargs').argv
+const appName = argv._[0]
 
-const appName = process.argv[2]
+const repository = () => {
+    if (argv.backend) return 'https://github.com/naveteam/back-boilerplate.git'
+    if (argv.typescript) return 'https://github.com/naveteam/react-boilerplate-typescript.git'
+    if (argv.next) return 'https://github.com/naveteam/next-boilerplate.git'
+    if (argv.native) return 'https://github.com/naveteam/react-native-template.git'
+    if (argv.gatsby) return 'https://github.com/naveteam/gatsby-template.git'
+    if (argv.storyblok) return 'https://github.com/naveteam/storyblok-gatsby-boilerplate.git'
+    return 'https://github.com/naveteam/react-boilerplate.git'
+}
 
 const run = async () => {
     if (!shell.which('git')) {
-        shell.echo('Sorry, this script requires git')
+        console.log('Esse script precisa do Git instalado para seu funcionamento'.red)
         shell.exit(1)
     }
 
-    shell.exec(`git clone https://github.com/naveteam/react-boilerplate.git ${appName}`)
+    if (!appName) {
+        console.log('Por favor informe o nome do repositório'.red)
+        shell.exit(1)
+    }
+
+    console.log('\nClonando o repositório'.cyan)
+    shell.exec(`git clone ${repository()} ${appName} -q`)
     shell.cd(appName)
 
-    shell.sed('-i', 'react-boilerplate', appName, 'package.json')
+    shell.sed('-i', /"name":\s?".+"/, `"name": "${appName}"`, 'package.json')
     
+    console.log('\nIniciando o repositório'.cyan)
     shell.rm('-rf', '.git')
     shell.exec('git init')
     shell.exec('git add .')
-    shell.exec('git commit -m "Initial commit using Create Nave App" --quiet')
+    shell.exec('git commit -m "Initial commit using Create Nave App" -q')
     
-    if (shell.which('yarn')) {
-        shell.exec('yarn')
-    } else {
-        shell.exec('npm install')
+    console.log('\nInstalando as dependências\n'.cyan)
+    if (!argv.native) {
+        if (shell.which('yarn')) {
+            shell.exec('yarn')
+        } else {
+            shell.exec('npm install')
+        }
     }
 
-    console.log("All done")
+    console.log()
+    console.log()
+    console.log('Seu projeto está pronto para ser iniciado!'.green)
+    console.log()
+    console.log('Para iniciar o desenvolvimento, execute')
+    console.log()
+    console.log(`  cd ${appName}`)
+    if (shell.which('yarn')) {
+        console.log('  yarn start')
+    } else {
+        console.log('  npm run start')
+    }
+    console.log()
+    console.log()
+    console.log('Divirta-se!'.rainbow)
+    console.log('#naveteam'.magenta)
 }
 
 run()
